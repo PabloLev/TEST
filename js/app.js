4da9bffacf2a988f61e6f2a740d68e858f875843
Pablo Levin <pablevin@gmail.com>
2021-09-29 18:43:53 -0300

    CSS 2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
 	$('.slider-container').slick({
 		dots: false,
-		arrows: true,
-		centerMode: false,
+		arrows: false,
+		centerMode: true,
 		infinite: true,
 		speed: 300,
 		fade: true,
 		centerPadding: '60px',
 		slidesToShow: 1,
+		mobileFirst: true,
 		responsive: [
 			{
 				breakpoint: 1280,
 				settings: {
-					arrows: false,
+					arrows: true,
 					// centerMode: true,
 					// centerPadding: '40px',
 					// slidesToShow: 3,
